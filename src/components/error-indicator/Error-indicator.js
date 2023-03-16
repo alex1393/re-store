984a808da02e5459f400869fbeae3a4fdf3dc2f8
alex1393 <jjbumers@gmail.com>
2023-03-15 21:58:17 -0400

    add some project structure

---
+++ src/components/error-indicator/Error-indicator.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./error-indicator.css";
+
+const ErrorIndicator = () => {
+  return <p>Error epta</p>;
+};
+
+export default ErrorIndicator;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log("HI");
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
