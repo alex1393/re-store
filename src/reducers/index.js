fd7f6e0b8654e984df5dc5bff830d6cc78469952
alex1393 <jjbumers@gmail.com>
2023-04-02 22:55:58 -0400

    add shopping cart component

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,16 +2,36 @@ const initialState = {
   books: [],
   loading: true,
   error: null,
+  cartItems: [
+    { id: 1, name: "Book1", count: 3, total: 150 },
+    { id: 2, name: "Book2", count: 2, total: 90 },
+  ],
+  orderTotal: 240,
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case "FETCH_BOOKS_REQUEST":
-      return { books: [], loading: true, error: null };
+      return {
+        ...state,
+        books: [],
+        loading: true,
+        error: null,
+      };
     case "FETCH_BOOKS_SUCCESS":
-      return { books: action.payload, loading: false, error: null };
+      return {
+        ...state,
+        books: action.payload,
+        loading: false,
+        error: null,
+      };
     case "FETCH_BOOKS_FAILURE":
-      return { books: [], loading: false, error: action.payload };
+      return {
+        ...state,
+        books: [],
+        loading: false,
+        error: action.payload,
+      };
     default:
       return state;
   }
